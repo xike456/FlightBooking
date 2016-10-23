0075680d8d6ff47bb436eb7aa3260fc27d42e86e
xike456 <xquang.dang@gmail.com>
2016-10-23 13:45:41 +0700

    -fixed add flight bookings

--- Back-end/models/FlightDetail.js
+++ Back-end/models/FlightDetail.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 
 var FlightDetailSchema = new mongoose.Schema({
-    flightId: String,
-    seat: String,
-    price: Number,
+    id: String,
     day: Date,
+    seatClass: String,
+    priceClass: String,
+    price: Number,
     booking: {type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }
 });
 
